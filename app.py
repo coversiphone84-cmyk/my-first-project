173edead63deacb905326b7ad70fbd2aecc76262
coversiphone84-cmyk <coversiphone84@gmail.com>
2026-07-11 04:17:15 +0500

    Updates  app.py

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ from google import genai
 st.set_page_config(page_title="Usman's Chatbot", layout="centered")
 st.title("🤖 Usman ka Apna AI Chatbot")
 
-API_KEY = "AQ.Ab8RN6LNtkwpiCicdHc6HXmPWxlkDono2fxSVBfonIBuEA2Sug"
+API_KEY = "AQ.Ab8RN6Kvg-WP3YgVLcG5OnNqJjW7_dDReMukfeLTdz3-hvhqyw"
 
-# Client initialize karein
+
 if "client" not in st.session_state:
     st.session_state.client = genai.Client(api_key=API_KEY)
+
 if "chat" not in st.session_state:
     st.session_state.chat = st.session_state.client.chats.create(model="gemini-2.5-flash")
 
@@ -22,19 +23,16 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # User input box
-if user_input := st.chat_input("Google AI se kuch bhi poochein..."):
+if user_input := st.chat_input("hi dude"):
     # User ka message screen par dikhaein
     with st.chat_message("user"):
         st.markdown(user_input)
     st.session_state.messages.append({"role": "user", "content": user_input})
 
-    try:
-        # AI se response lein
-        response = st.session_state.chat.send_message(user_input)
-        
-        # AI ka response screen par dikhaein
-        with st.chat_message("assistant"):
-            st.markdown(response.text)
-        st.session_state.messages.append({"role": "assistant", "content": response.text})
-    except Exception as e:
-        st.error(f"Error aaya hai: {e}")
+    # AI se response lein
+    response = st.session_state.chat.send_message(user_input)
+    
+    # AI ka response screen par dikhaein
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
+    st.session_state.messages.append({"role": "assistant", "content": response.text})
