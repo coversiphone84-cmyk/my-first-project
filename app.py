ba64168c4974db0823392328b8433e4cd0770183
coversiphone84-cmyk <coversiphone84@gmail.com>
2026-07-11 01:48:32 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from google import genai
+
+
+st.set_page_config(page_title="Usman's Chatbot", layout="centered")
+st.title("🤖 Usman ka Apna AI Chatbot")
+
+
+API_KEY = "AQ.Ab8RN6INVnP-ybwcIcUYrWDALFw42cjtwtpIpurpfjhh_sxSwg"
+
+
+if "client" not in st.session_state:
+    st.session_state.client = genai.Client(api_key=API_KEY)
+
+if "chat" not in st.session_state:
+    st.session_state.chat = st.session_state.client.chats.create(model="gemini-2.5-flash")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Purani baatein screen par dikhane ke liye loop
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# User input box
+if user_input := st.chat_input("hi dude"):
+    # User ka message screen par dikhaein
+    with st.chat_message("user"):
+        st.markdown(user_input)
+    st.session_state.messages.append({"role": "user", "content": user_input})
+
+    # AI se response leiny k liaaa
+    response = st.session_state.chat.send_message(user_input)
+    
+    # AI ka response screen par dikhaein
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
+    st.session_state.messages.append({"role": "assistant", "content": response.text})
